ed8b8176123afc04f51f10c9b6fc56d645f1d4f1
insanetron75 <timmy_hasler@hotmail.com>
2020-01-22 17:50:43 +0000

    Change Title
    Had the wrong title..whoops!

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 import Players from "./components/Players";
 import "./styles.css";
 import getPlayers from "./api/getPlayers";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function App() {
   const players = getPlayers();
   return (
     <div className="App">
-      <h1>Float Test</h1>
+      <h1>FanDuel Test</h1>
       <Players players={players} />
     </div>
   );
