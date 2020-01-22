c564ae87e34dcd1f49e398e55bf22e1945d110b3
insanetron75 <timmy_hasler@hotmail.com>
2020-01-22 13:24:55 +0000

    API get
    Added the full get method

--- src/api/getPlayers.js
+++ src/api/getPlayers.js
@@ -1,8 +1,12 @@
-const fetch = require("node-fetch");
+import axios from "axios";
 
 const getPlayers = async () => {
-  let players = {};
-  return players;
+  const url =
+    "https://gist.githubusercontent.com/liamjdouglas/bb40ee8721f1a9313c22c6ea0851a105/raw/6b6fc89d55ebe4d9b05c1469349af33651d7e7f1/Player.json";
+
+  return axios.get(url).then(response => {
+    return response.data.players;
+  });
 };
 
 export default getPlayers;
--- src/tests/api/getPlayers.test.js
+++ src/tests/api/getPlayers.test.js
@@ -17,11 +17,28 @@ describe("Test the API Functions", () => {
             "https://d17odppiik753x.cloudfront.net/playerimages/nba/9524.png",
           width: 200
         }
+      },
+      injured: false,
+      injury_details: "knee",
+      injury_status: "o",
+      last_name: "Curry",
+      news: {
+        latest: "2016-05-02T18:35:15Z"
+      },
+      played: 79,
+      player_card_url: "https://www.fanduel.com/e/Player/9524/Stats/15475",
+      position: "PG",
+      removed: false,
+      salary: 10600,
+      starting_order: null,
+      team: {
+        _members: ["687"],
+        _ref: "teams.id"
       }
     };
 
     const results = await getPlayers();
 
-    expect(results["players"][0]).toEqual(expectedResults);
+    expect(results[0]).toEqual(expectedResults);
   });
 });
