6537d932e24e446069c38f8733ca08b664d981e5
Timothy Hasler <timmy_hasler@hotmail.com>
2020-01-22 17:00:52 +0000

    select player

--- src/components/Players/index.js
+++ src/components/Players/index.js
@@ -3,20 +3,22 @@ import getPlayers from "../../api/getPlayers";
 import shuffle from "../../functions/shuffle";
 import {
   Container,
-  Card,
   Col,
-  Row
+  Row,
+  Button
 } from 'react-bootstrap';
 import Player from "../Player";
 
 class Players extends Component {
   constructor() {
     super();
+    this.handleClick = this.handleClick.bind(this);
     this.state = {
       players: [
         {first_name: 'Loading...'},
         {first_name: 'Loading...'}
-      ]
+      ],
+      count: 0
     };
   }
 
@@ -39,6 +41,36 @@ class Players extends Component {
     }
   }
 
+  handleClick(e) {
+    console.log(this.state);
+    const players = [
+      this.createPlayer('', this.state.players[0]),
+      this.createPlayer('', this.state.players[1]),
+    ]
+  
+    const currentState = this.state;
+    const result = (e.target.id === "player_1_btn") 
+      ? this.compare(players[0], players[1])
+      : this.compare(players[1], players[0]);
+
+    if (result) {
+      currentState.count = currentState.count + 1;
+      this.setState(currentState)
+    } else {
+      alert('WRONG!');
+    }
+
+    this.componentDidMount();
+  }
+
+  compare(selectedPlayer, otherPlayer) {
+    const result = selectedPlayer.score > otherPlayer.score;
+    console.log(selectedPlayer.score)
+    console.log(otherPlayer.score)
+    console.log(result);
+    return selectedPlayer.score > otherPlayer.score
+  }
+
   render() {
     const firstPlayer = this.createPlayer('Player 1', this.state.players[0]);
     const secondPlayer = this.createPlayer('Player 2',this.state.players[1]);
@@ -46,10 +78,29 @@ class Players extends Component {
       <Container>
         <Row>
           <Col>
-            <Player player={firstPlayer}></Player>
+            <Player player={ firstPlayer }></Player>
           </Col>
           <Col>
-            <Player player={secondPlayer}></Player>
+            <Player player={ secondPlayer }></Player>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button 
+              id="player_1_btn"
+              className="mt-1"
+              variant="success"
+              onClick={ this.handleClick }
+            >
+              Select Player
+            </Button>
+          </Col>
+          <Col>
+            <Button 
+              className="mt-1"
+              variant="success"
+              onClick={ this.handleClick }
+            >Select Player </Button>
           </Col>
         </Row>
       </Container>
