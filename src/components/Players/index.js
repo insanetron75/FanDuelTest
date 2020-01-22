4c83ac70471490c00a3050c4b25ff178f4d19caf
Timothy Hasler <timmy_hasler@hotmail.com>
2020-01-22 17:37:29 +0000

    add rapid click protection

--- src/components/Players/index.js
+++ src/components/Players/index.js
@@ -19,16 +19,26 @@ class Players extends Component {
         {first_name: 'Loading...'},
         {first_name: 'Loading...'}
       ],
-      count: 0
+      count: 0,
+      result: {
+        selectedPlayerScore: 0,
+        otherPlayerScore: 0,
+        message: "You haven't guessed yet"
+      },
+      loading: false
     };
   }
 
   componentDidMount() {
+    this.setState({ loading: true });
     getPlayers().then((players) => {
       players = shuffle(players);
       this.setState({
-        players: shuffle(players)
+        players: shuffle(players),
       });
+      this.setState({
+        loading: false
+      })
     });
   }
 
@@ -53,31 +63,54 @@ class Players extends Component {
       ? this.compare(players[0], players[1])
       : this.compare(players[1], players[0]);
 
-    if (result) {
+    if (result.result) {
       currentState.count = currentState.count + 1;
-      this.setState(currentState)
-    } else {
-      alert('WRONG!');
+      if (currentState.count === 10) {
+        alert('you win!');
+      }
     }
+
+    currentState.result = result;
+    this.setState(currentState)
 
     this.componentDidMount();
   }
 
-  compare = (selectedPlayer, otherPlayer) => (selectedPlayer.score > otherPlayer.score)
+  compare(selectedPlayer, otherPlayer) {
+   return {
+      selectedPlayerScore: selectedPlayer.score.toFixed(2),
+      otherPlayerScore: otherPlayer.score.toFixed(2),
+      message: selectedPlayer.score > otherPlayer.score ? "Correct!" : "Wrong!",
+      result: selectedPlayer.score > otherPlayer.score
+    }
+  }
 
+  isLoading() {
+    console.log(this.state.loading);
+    return this.state.loading;
+  }
   render() {
     const firstPlayer = this.createPlayer('Player 1', this.state.players[0]);
     const secondPlayer = this.createPlayer('Player 2',this.state.players[1]);
     return (
       <Container>
         <Row className="mt-1">
-          <Col></Col>
-            <Col>
-              <Card bg="success">
-                <Card.Header>Score:</Card.Header>
-                <Card.Title>{this.state.count}</Card.Title>
-              </Card>
-            </Col>
+          <Col>
+            <Card>
+              <Card.Header>Result</Card.Header>
+              <Card.Title>{ this.state.result.message }</Card.Title>
+              <Card.Body>
+                Selected Player Score: { this.state.result.selectedPlayerScore }<br></br>
+                Other Player Score: { this.state.result.otherPlayerScore }
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card bg="success">
+              <Card.Header>Score:</Card.Header>
+              <Card.Title>{this.state.count}</Card.Title>
+            </Card>
+          </Col>
           <Col></Col>
         </Row>
         <Row>
@@ -94,7 +127,7 @@ class Players extends Component {
               id="player_1_btn"
               className="mt-1"
               variant="success"
-              onClick={ this.handleClick }
+              onClick={ this.isLoading ? this.handleClick : null }
             >
               Select Player
             </Button>
@@ -103,7 +136,7 @@ class Players extends Component {
             <Button 
               className="mt-1"
               variant="success"
-              onClick={ this.handleClick }
+              onClick={ this.isLoading ? this.handleClick : null }
             >Select Player </Button>
           </Col>
         </Row>
